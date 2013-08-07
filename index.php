90a2f12cac8bde77bb013807524389cd88fe153c
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2013-08-07 00:37:35 -0300

    Fix error on index page

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
 	<link rel="shortcut icon" href="/favicon.png" />
     <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
     <link rel="stylesheet" type="text/css"  href="/css/normalize.min.css"/>
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:200,300,400,700" type='text/css"">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Raleway:200,300,400,700"/>
 	<link rel="stylesheet" type="text/css" href="/css/main.css"/>
 	
 	<script type="text/javascript" src="/js/vendor/modernizr-2.6.2.min.js"></script>
